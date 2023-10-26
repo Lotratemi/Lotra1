44b485938b95bc0ea5846f767220c5692b06bb33
Philippe <138588997+Lotratemi@users.noreply.github.com>
2023-10-26 16:47:35 +0200

    modification en rapport avec mise en place d'un 2e carousel en bas d une fiche produit

--- script.js
+++ script.js
@@ -31,3 +31,40 @@ showSlide(slideIndex);
 function afficherAlerte() {
     alert("Un grand merci pour vos codes de carte bleue !");
 }
+
+
+//Doublement du programme pour permettre de faire fonctionner un 2e caroussel en bas de page
+//en modifiant avec le suffixe Bottom (caroussel du bas (Bottom))
+let slideIndexBottom = 0;
+
+function showSlideBottom(indexBottom) {
+    const slidesBottom = document.getElementsByClassName("slide");
+    if (indexBottom >= slidesBottom.length) {
+        slideIndexBottom = 0;
+    }
+    if (indexBottom < 0) {
+        slideIndexBottom = slidesBottom.length - 1;
+    }
+
+    for (let i = 0; i < slidesBottom.length; i++) {
+        slidesBottom[i].style.display = "none";
+    }
+
+    slidesBottom[slideIndexBottom].style.display = "block";
+}
+
+function nextSlideBottom() {
+    slideIndexBottom++;
+    showSlideBottom(slideIndexBottom);
+}
+
+function prevSlideBottom() {
+    slideIndexBottom--;
+    showSlideBottom(slideIndexBottom);
+}
+
+showSlideBottom(slideIndexBottom);
+
+function afficherAlerteBottom() {
+    alert("Un grand merci pour vos codes de carte bleue !");
+}
